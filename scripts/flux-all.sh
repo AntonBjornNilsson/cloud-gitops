cce4662f1ba73c6ac64e9746271812cc68c25689
AntonBjornNilsson <contact@antonbjornnilsson.com>
2026-05-01 21:21:01 +0200

    Start migration to traefik from nginx

---
+++ scripts/flux-all.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+if [[ ($1 != 'suspend') && ($1 != 'resume') ]]; then
+    echo "$1 whas not correct"
+    exit 0
+fi;
+
+for i in $(kubectl get kustomizations.kustomize.toolkit.fluxcd.io -n flux-system -o name | cut -d "/" -f2); do
+    flux $1 kustomization -n flux-system $i
+done
